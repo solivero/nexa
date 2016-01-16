aa6c1da0dac70c6e11f6cb59cf9713830ade29a4
solivero <oliver.ce.petri@gmail.com>
2016-01-16 01:06:33 +0100

    Ajax buttons

--- app.py
+++ app.py
@@ -2,34 +2,23 @@ from flask import Flask, render_template, redirect
 from subprocess import call
 app = Flask(__name__)
 app.debug = True
-states= [False, False, False]
 
 pin = 0
 transmitter = 1235262
 
 @app.route("/")
 def main():
-    return render_template('index.html', states=states)
+    return render_template('index.html')
 
 @app.route("/on/<int:id>")
 def on(id=0):
     send_signal(True, id-1)
-    global states
-    if id != 0:
-        states[id-1] = True
-    else:
-        states = [True, True, True]
     return redirect("/")
     #return render_template('index..html', on=state)
 
 @app.route("/off/<int:id>")
 def off(id=0):
     send_signal(False, id-1)
-    global states
-    if id != 0:
-        states[id-1] = False
-    else:
-        states = [False, False, False]
     return redirect("/")
     #return render_template('index..html', on=state)
 
